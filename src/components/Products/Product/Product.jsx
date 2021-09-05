875db87de1c1fd31e2343e59f5bbd32d81d316b8
Raju Dhakal <wrazu19@gmail.com>
2021-09-05 11:22:07 +0545

    E-Commerce Website Init

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
+
+
+const Product = () => {
+    return (
+        <Card className={classes.root}  >
+            <CardMedia className={classes.media} image='' title={product.name}/>
+            <CardContent>
+                <div className={classes.cardContent}></div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Product
